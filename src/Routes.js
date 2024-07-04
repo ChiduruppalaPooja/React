b4cd504cb0d12c2d540331b39b323364f20cdd24
rbs-pooja <cpoojareddy2019@gmail.com>
2024-07-04 09:38:54 +0530

    deploy

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Dashboard from './pages/Dashboard/Dashboard';
 import AuthRoutes from './components/common/AuthRoutes';
